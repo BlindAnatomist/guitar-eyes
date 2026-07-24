2f27693c3841e6ea202eeee30e2e162cbe2624d3
BlindAnatomist <132391374+BlindAnatomist@users.noreply.github.com>
2026-07-24 14:05:28 -0500

    fix: make upload state explicit and accessible

--- src/Upload.js
+++ src/Upload.js
@@ -1,19 +1,26 @@
-import React, { useRef } from "react";
+import React from "react";
 
- function Upload({ onFileUpload }) {
- const fileInputRef = useRef(null);
- const handleFileChange = (e) => {
- const file = e.target.files[0];
- if (file) { onFileUpload(file);
- }
- };
+function Upload({ onFileUpload, disabled = false }) {
+  const handleFileChange = (event) => {
+    const file = event.target.files?.[0];
 
- return (
- <div>
- <label htmlFor="file-upload">
-Upload .txt file:</label> <input id="file-upload" type="file" accept=".txt" ref={fileInputRef} onChange={handleFileChange} />
- </div>
- );
- }
+    if (file) {
+      onFileUpload(file);
+    }
+  };
 
- export default Upload;
+  return (
+    <div className="upload-control">
+      <label htmlFor="file-upload">Upload .txt file:</label>{" "}
+      <input
+        id="file-upload"
+        type="file"
+        accept=".txt,text/plain"
+        disabled={disabled}
+        onChange={handleFileChange}
+      />
+    </div>
+  );
+}
+
+export default Upload;
