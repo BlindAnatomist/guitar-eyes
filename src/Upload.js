257f66b3ea3934c5c7e17507c9fca9a2599cd3bc
BlindAnatomist <132391374+BlindAnatomist@users.noreply.github.com>
2026-07-26 01:09:35 -0500

    Allow recognized tablature file families in picker

--- src/Upload.js
+++ src/Upload.js
@@ -11,11 +11,11 @@ function Upload({ onFileUpload, disabled = false }) {
 
   return (
     <div className="upload-control">
-      <label htmlFor="file-upload">Upload .txt file:</label>{" "}
+      <label htmlFor="file-upload">Upload tablature file:</label>{" "}
       <input
         id="file-upload"
         type="file"
-        accept=".txt,text/plain"
+        accept=".txt,.tab,.musicxml,.xml,.mxl,.gtp,.gp3,.gp4,.gp5,.gpx,.gp,.ptb,.pt2,.tg,.tef,text/plain,application/xml,text/xml"
         disabled={disabled}
         onChange={handleFileChange}
       />
