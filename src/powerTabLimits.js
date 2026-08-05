ec215139553cd1b90dd9eb55c687e6c2f977fa56
BlindAnatomist <132391374+BlindAnatomist@users.noreply.github.com>
2026-08-05 11:43:52 -0500

    Stage PowerTab resource limits

---
+++ src/powerTabLimits.js
@@ -0,0 +1,11 @@
+export const POWERTAB_LIMITS = Object.freeze({
+  maxCompressedBytes: 5 * 1024 * 1024,
+  maxDecompressedBytes: 20 * 1024 * 1024,
+  maxSystems: 256,
+  maxPlayers: 32,
+  maxInstruments: 64,
+  maxStaves: 64,
+  maxMeasures: 4096,
+  maxPositions: 50000,
+  maxNotes: 200000,
+});
